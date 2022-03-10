aeb72ff4484fdedce034b8cb55d016e84c9c5868
marco maione <maionemarco14@gmail.com>
2022-03-10 16:42:32 +0100

    snack-5

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
 $stringa = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore quasi possimus facilis voluptas blanditiis. dolorum sint tenetur voluptatibus perspiciatis at quae itaque iste porro corporis quam. voluptatem excepturi commodi corrupti";
 $parti = explode(".", $stringa);
 
-echo $parti."<br>";
+for ($i=0; $i < count($parti); $i ++) {
+    echo $parti[$i] . '<br>';
+}
 
 
 ?>
