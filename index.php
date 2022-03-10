3f5db86449546d6513ad940b2c1acd4c292dcb49
marco maione <maionemarco14@gmail.com>
2022-03-10 16:23:45 +0100

    aggiornamento

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+$stringa = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore quasi possimus facilis voluptas blanditiis. dolorum sint tenetur voluptatibus perspiciatis at quae itaque iste porro corporis quam. voluptatem excepturi commodi corrupti";
+$parti = explode(".", $stringa);
+
+echo $parti."<br>";
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>snacks-b1</title>
+</head>
+<body>
+    
+</body>
+</html>
